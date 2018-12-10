c7874e17213bccdd49741d1d8e6c5597a1d47b5f
Archie Hicklin <afphicklin@gmail.com>
2018-12-10 12:49:51 +0000

    card styling

--- src/App.js
+++ src/App.js
@@ -357,7 +357,12 @@ class App extends Component {
       <div className={styles.template}>
         {Sections}
       </div >
-      <div className={styles.cardConatiner}>An AI dental assistant.</div>
+      <div className={styles.cardConatiner}>
+      <div className={styles.cardImage}></div>
+        <div className={styles.cardTitle}>An AI dental assistant.</div>
+        <div className={styles.cardParagraph}> Kiroku lets you spend significantly less time writing clinical notes.</div>
+        <button className={styles.cardButton}>Learn more</button>
+        </div>
       </div>
     );
   }
