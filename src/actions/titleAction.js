8df98ee88c6283fa9482a9ede26027f1fde1b558
Jay Shah <jayshah14js@gmail.com>
2018-12-11 12:54:33 +0000

    abstracted section

---
+++ src/actions/titleAction.js
@@ -0,0 +1,10 @@
+export const mouseEnterTrashTitleIcon = (key) => ({
+    type: 'MOUSE_ENTER_TRASH_TITLE_ICON',
+    key: key
+})
+
+export const mouseLeaveTrashTitleIcon = () => ({
+    type: 'MOUSE_LEAVE_TRASH_TITLE_ICON'
+})
+
+
