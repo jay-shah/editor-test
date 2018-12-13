943d28ff5b89f65f2baf87d9bc254e978cebc9e4
Jay Shah <jayshah14js@gmail.com>
2018-12-13 16:16:33 +0000

    hide modal on cross click

--- src/actions/signUpAction.js
+++ src/actions/signUpAction.js
@@ -1,3 +1,8 @@
 export const showModal = () => ({
     type: 'SHOW_MODAL'
 })
+
+export const hideModal = () => ({
+    type: 'HIDE_MODAL'
+})
+
--- src/components/LearnMoreCard/LearnMoreCard.jsx
+++ src/components/LearnMoreCard/LearnMoreCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './LearnMoreCard.module.css'
 
-const LearnMoreCard = () => {
+const LearnMoreCard = ({ showModal }) => {
     return (
         <div className={styles.cardConatiner}>
             <div className={styles.cardImage}></div>
             <div className={styles.cardTitle}>An AI dental assistant.</div>
             <div className={styles.cardParagraph}> Kiroku lets you spend significantly less time writing clinical notes.</div>
-            <div><button className={styles.cardButton}>Learn more</button></div>
+            <div><button className={styles.cardButton} onClick={showModal}>Learn more</button></div>
         </div>
     )
 }
