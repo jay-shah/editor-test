4f2a9f3f3aee42c36a50a3194f31543a033aa018
Jay Shah <jayshah14js@gmail.com>
2018-12-13 21:16:13 +0000

    fixed href warning

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import SignUpButton from './SignUpButton'
 const NavBar = () => {
     return (
         <div className={styles.navBar}>
-            <a href="#"><div className={styles.kirokuLogo}></div></a>
+            <a href="#home"><div className={styles.kirokuLogo}></div></a>
             <SignUpButton />
             <a href="mailto:hannah@trykiroku.com?subject=Equiry about Kiroku" target="_blank" rel="noopener noreferrer"><div className={styles.navList}>Contact</div></a>
             <a href="https://www.trykiroku.com" target="_blank" rel="noopener noreferrer"><div className={styles.navList}>About</div></a>
